0406c1ef3d88602d7ccb2ee73f6563eaf85ddbba
ronan <ronan.paugam@pm.me>
2025-05-13 23:33:05 +0200

    update log transfer

--- src/run-fwi.sh
+++ src/run-fwi.sh
@@ -12,6 +12,7 @@ rm $dataDir/FWI/timeToCompute.txt
 
 #ffmpeg could not work in the cron on the server, I used the file create below to trigger ffmpeg on another computer.
 touch $dataDir/FWI/PLOT_latest/run_ffmpeg_on_moor
+cp logDir/fwi.log /home/paugam/WebSite/certec/data/FWI/fwi.log
 #python /home/paugam/bin/render_anim/render_anim.py -i /mnt/data3/SILEX/AROME/FWI/PLOT_latest/png/ -ext png -o /mnt/data3/SILEX/AROME/FWI/PLOT_latest/fwi_latest.avi -fr 1
 #ffmpeg -i /mnt/data3/SILEX/AROME/FWI/PLOT_latest/fwi_latest.avi /mnt/data3/SILEX/AROME/FWI/PLOT_latest/fwi_latest.mp4
 
